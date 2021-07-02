d8596af073060385541d0fdee1c7b180dcba6652
jayChung0302 <whtnek@gmail.com>
2021-07-02 16:08:51 +0900

    Add feature regularization loss and pixel correlation loss

--- deep-dream.py
+++ deep-dream.py
@@ -1,4 +1,3 @@
-import sys
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -8,37 +7,106 @@ import torch.optim as optim
 import numpy as np
 import mypy
 import random
+import sys
 from tqdm import tqdm
 from torch.utils.tensorboard import SummaryWriter
 import argparse
+# import scipy.ndimage as nd
+from utils import preprocess, deprocess, clip, FeatureRegHook
 
 random.seed(0)
 parser = argparse.ArgumentParser()
 parser.add_argument('--exp_name', type=str, help='experiment name', default='dryrun')
+parser.add_argument('--correlate_scale', type=float, help='', default='2.5e-5')
+parser.add_argument('--bn_reg_scale', type=float, help='', default='1e2')
+parser.add_argument('--l2_scale', type=float, help='', default='0.0')
+parser.add_argument('--num_epoch', type=int, help='', default='500')
 
+args = parser.parse_args()
 
-def dream_loss():
-    pass
+def dream(image, model, iterations, lr):
+    """ Updates the image to maximize outputs for n iterations """
+    Tensor = torch.cuda.FloatTensor if torch.cuda.is_available else torch.FloatTensor
+    image = Variable(Tensor(image), requires_grad=True)
+    for i in range(iterations):
+        model.zero_grad()
+        out = model(image)
+        loss = out.norm()
+        loss.backward()
+        avg_grad = np.abs(image.grad.data.cpu().numpy()).mean()
+        norm_lr = lr / avg_grad
+        image.data += norm_lr * image.grad.data
+        image.data = clip(image.data)
+        image.grad.data.zero_()
+    return image.cpu().data.numpy()
 
+def deep_dream(image, model, iterations, lr, octave_scale, num_octaves):
+    """ Main deep dream method """
+    image = preprocess(image).unsqueeze(0).cpu().data.numpy()
 
+    # Extract image representations for each octave
+    octaves = [image]
+    for _ in range(num_octaves - 1):
+        octaves.append(nd.zoom(octaves[-1], (1, 1, 1 / octave_scale, 1 / octave_scale), order=1))
 
-if __name__ == '__main__':
-    args = parser.parse_args()
+    detail = np.zeros_like(octaves[-1])
+    for octave, octave_base in enumerate(tqdm.tqdm(octaves[::-1], desc="Dreaming")):
+        if octave > 0:
+            # Upsample detail to new octave dimension
+            detail = nd.zoom(detail, np.array(octave_base.shape) / np.array(detail.shape), order=1)
+        # Add deep dream detail from previous octave to new base
+        input_image = octave_base + detail
+        # Get new deep dream image
+        dreamed_image = dream(input_image, model, iterations, lr)
+        # Extract deep dream details
+        detail = dreamed_image - octave_base
+
+    return deprocess(dreamed_image)
+
+def main():
+    lim_0, lim_1 = 2, 2
     noise = torch.rand((1,3,224,224), requires_grad=True, device='cpu')
     loss_fn = nn.CrossEntropyLoss()
-    net = models.mobilenet_v2(pretrained=True)
+    net = models.resnet50(pretrained=True)
     optimizer = optim.Adam([noise], lr=0.01, betas=[0.5, 0.9], eps = 1e-8)
     net.eval()
     writer = SummaryWriter(f'logs/{args.exp_name}')
     target = torch.tensor([1])
-    for i in tqdm(range(500)):
-        outputs = net(noise)
+
+    loss_r_feature_layers = []
+    for module in net.modules():
+        if isinstance(module, nn.BatchNorm2d):
+            loss_r_feature_layers.append(FeatureRegHook(module))
+
+    for i in tqdm(range(args.num_epoch)):
+        net.zero_grad()
+        optimizer.zero_grad()
+
+        off1 = random.randint(-lim_0, lim_0)
+        off2 = random.randint(-lim_1, lim_1)
+        inputs_jit = torch.roll(noise, shifts=(off1,off2), dims=(2,3))
+
+        outputs = net(inputs_jit)
         loss = loss_fn(outputs, target)
+        
+        # nearby pixels should be correlated
+        diff1 = inputs_jit[:,:,:,:-1] - inputs_jit[:,:,:,1:]
+        diff2 = inputs_jit[:,:,:-1,:] - inputs_jit[:,:,1:,:]
+        diff3 = inputs_jit[:,:,1:,:-1] - inputs_jit[:,:,:-1,1:]
+        diff4 = inputs_jit[:,:,:-1,:-1] - inputs_jit[:,:,1:,1:]
+
+        loss_var = torch.norm(diff1) + torch.norm(diff2) + torch.norm(diff3) + torch.norm(diff4)
+        loss = loss + args.correlate_scale * loss_var
+        loss_distr = sum([mod.r_feature for mod in loss_r_feature_layers])
+        loss = loss + args.bn_reg_scale * loss_distr # best for noise before BN
+        loss = loss + args.l2_scale * torch.norm(inputs_jit, 2)
         loss.backward()
         optimizer.step()
-        noise_grid = torchvision.utils.make_grid(noise)
+        inputs_grid = torchvision.utils.make_grid(noise)
         if i%10 == 0:
             writer.add_scalar('training loss',
                 loss, i)
-            writer.add_image('optimized_noise', noise_grid, i)
-    
+            writer.add_image('optimized_noise', inputs_grid, i)
+
+if __name__ == '__main__':
+    main()
--- utils.py
+++ utils.py
@@ -1,6 +1,16 @@
-# Mosaic
+import torch
+import torchvision.transforms as transforms
 import cv2
+import numpy as np
 
+mean = np.array([0.485, 0.456, 0.406])
+std = np.array([0.229, 0.224, 0.225])
+preprocess = transforms.Compose(
+    [transforms.ToTensor(),
+    transforms.Normalize(mean, std),
+    ])
+
+# Mosaic
 def mosaic(img, ratio=0.1):
     small = cv2.resize(img, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
     return cv2.resize(small, img.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
@@ -10,7 +20,49 @@ def mosaic_area(img, x, y, width, height, ratio=0.1):
     dst[y:y + height, x:x + width] = mosaic(dst[y:y + height, x:x + width], ratio)
     return dst
 
+def deprocess(image_np):
+    image_np = image_np.squeeze().transpose(1, 2, 0)
+    image_np = image_np * std.reshape((1, 1, 3)) + mean.reshape((1, 1, 3))
+    image_np = np.clip(image_np, 0.0, 255.0)
+    return image_np
+
+
+def clip(image_tensor):
+    for c in range(3):
+        m, s = mean[c], std[c]
+        image_tensor[0, c] = torch.clamp(image_tensor[0, c], -m / s, (1 - m) / s)
+    return image_tensor
+
+class FeatureRegHook():
+    '''
+    https://arxiv.org/abs/1912.08795
+    Implementation of the forward hook to track feature statistics and compute a loss on them.
+    Will compute mean and variance, and will use l2 as a loss
+    '''
+
+    def __init__(self, module: torch.nn):
+        self.hook = module.register_forward_hook(self.hook_fn)
+
+    def hook_fn(self, module, input, output):
+        # hook co compute deepinversion's feature distribution regularization
+        nch = input[0].shape[1]
+
+        mean = input[0].mean([0, 2, 3])
+        var = input[0].permute(1, 0, 2, 3).contiguous().view([nch, -1]).var(1, unbiased=False)
+
+        # forcing mean and variance to match between two distributions
+        # other ways might work better, e.g. KL divergence
+        r_feature = torch.norm(module.running_var.data.type(var.type()) - var, 2) + torch.norm(
+            module.running_mean.data.type(var.type()) - mean, 2)
+
+        self.r_feature = r_feature
+        # must have no output
+
+    def close(self):
+        self.hook.remove()
+
 if __name__ == '__main__':
     img = cv2.imread('img/Lenna.png')
     dst_area = mosaic_area(img, 220, 230, 150, 150)
     cv2.imwrite('cv-mosaic.jpeg', dst_area)
+    
