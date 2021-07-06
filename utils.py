0971bf615889cf07c22dc46014931578e43c3147
jayChung0302 <whtnek@gmail.com>
2021-07-06 17:31:58 +0900

    wip adding feature regularization on group norm layer

--- utils.py
+++ utils.py
@@ -34,7 +34,7 @@ def clip(image_tensor):
         image_tensor[0, c] = torch.clamp(image_tensor[0, c], -m / s, (1 - m) / s)
     return image_tensor
 
-class FeatureRegHook():
+class FeatureRegHook:
     
     def __init__(self, module: torch.nn):
         self.hook = module.register_forward_hook(self.hook_fn)
@@ -51,32 +51,37 @@ class BatchHook(FeatureRegHook):
     Implementation of the forward hook to track feature statistics and compute a loss on them.
     Will compute mean and variance, and will use l2 as a loss
     '''
-    def __init__(self):
-        super(BatchHook, self).__init__(self)
+    def __init__(self, module: torch.nn):
+        super(BatchHook, self).__init__(module)
 
     def hook_fn(self, module, input, output):
-        # hook co compute deepinversion's feature distribution regularization
         nch = input[0].shape[1]
+        input = input[0].permute(1, 0, 2, 3).contiguous().view(nch, -1)
+        mean = input.mean(dim=1)
+        var = input.var(dim=1, unbiased=False)
 
-        mean = input[0].mean([0, 2, 3])
-        var = input[0].permute(1, 0, 2, 3).contiguous().view([nch, -1]).var(1, unbiased=False)
-
-        # forcing mean and variance to match between two distributions
-        # other ways might work better, e.g. KL divergence
-        r_feature = torch.norm(module.running_var.data.type(var.type()) - var, 2) + torch.norm(
-            module.running_mean.data.type(var.type()) - mean, 2)
+        r_feature = torch.norm(module.running_var - var, 2) + torch.norm(
+            module.running_mean - mean, 2)
 
         self.r_feature = r_feature
-        # must have no output
         
 
 class GroupHook(FeatureRegHook):
-    def __init__(self):
-        super(GroupHook, self).__init__(self)
+    def __init__(self, module: torch.nn):
+        super(GroupHook, self).__init__(module)
+        self.ngroups = module.num_groups
     
     def hook_fn(self, module, input, output):
-        pass
+        nch = input[0].shape[1]
+        input = input[0].contiguous().view(nch, self.ngroups, -1)
+        mean = input.mean(dim=2)
+        var = input.var(dim=2, unbiased=False)
 
+        r_feature = torch.norm(module.running_var - var, 2) + torch.norm(
+            module.running_mean - mean, 2)
+
+        self.r_feature = r_feature
+        
 if __name__ == '__main__':
     img = cv2.imread('img/Lenna.png')
     dst_area = mosaic_area(img, 220, 230, 150, 150)
